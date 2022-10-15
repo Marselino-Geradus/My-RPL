7d61de98c2ede2e20334007547385476ed9de477
Marselino-Geradus <marselino914@gmail.com>
2022-10-15 11:12:57 +0800

    First commit

---
+++ FileJS.js
@@ -0,0 +1,35 @@
+// alert("SELAMAT DATANG MAS BRO^_^");
+// var nama;
+// nama="Ramdhani";
+
+// console.log(nama);
+// console.log("Nama teman saya: "+nama);
+
+// var b = prompt("Masukkan nama :");
+// console.log(b);
+// document.write("Hai "+b);
+
+console.log(">>>CARA PERTAMA<<<");
+console.log("Perkenalan diri");
+console.log("Nama  : Marselino Geradus.");
+console.log("Kelas : Informatika_G.");
+console.log("NIM   : D0221364.");
+
+
+
+var nama = "Marselino Geradus.",
+	nim = "D0221364.",
+	kelas = "Informatika_G.";
+	asalSekolah = "SMAN 1 Balla";
+console.log("___________________________");
+console.log(">>>CARA KEDUA<<<");
+console.log("Perkenalan Diri");
+console.log("Nama  : "+nama);
+console.log("NIM   : "+nim);
+console.log("Kelas : "+kelas);
+console.log("Nama Sekolah: "+asalSekolah);
+
+
+
+
+
